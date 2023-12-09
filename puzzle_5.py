1c046075ee9e63c94b56583f116d5253e1fa53a4
Aaron Berdy <berdy@amazon.com>
2023-12-09 13:19:16 -0800

    clean up day 5

--- puzzle_5.py
+++ puzzle_5.py
@@ -10,15 +10,6 @@ class Range:
         self.start = start
         self.end = end
 
-    def __bool__(self):
-        return self.end > self.start
-
-    def __add__(self, other):
-        return Range(self.start + other, self.end + other)
-
-    def __sub__(self, other):
-        return self + (-other)
-
     def __repr__(self):
         return f"Range({self.start}, {self.end})"
 
@@ -29,33 +20,16 @@ class Range:
 class Transform:
 
     def __init__(self, dest_start: int, source_start: int, range_len: int):
-        self.dest_start = dest_start
-        self.dest_end = dest_start + range_len
-        self.dest_range = Range(self.dest_start, self.dest_end)
         self.source_start = source_start
         self.source_end = source_start + range_len
         self.source_range = Range(self.source_start, self.source_end)
-        self.range_len = range_len
-        self.offset = self.dest_start - self.source_start
-
-    @classmethod
-    def from_range_and_offset(cls, start, end, offset):
-        return cls(start + offset, start, end - start)
-
-    def __lt__(self, other):
-        return self.source_start < other.source_start
+        self.offset = dest_start - self.source_start
 
     def __str__(self):
-        return f"[{self.source_start}, {self.source_start + self.range_len}){self.offset:+}"
+        return f"[{self.source_start}, {self.source_end}){self.offset:+}"
 
     def __repr__(self):
         return str(self)
-
-    def __eq__(self, other):
-        return str(self) == str(other)
-
-    def __hash__(self):
-        return hash(str(self))
 
     def __call__(self, source_num: int | Range):
         if isinstance(source_num, Range):
@@ -75,47 +49,30 @@ class Puzzle5Solver(Solver):
 
     def process_seeds(self, seed_str):
         if self.puzzle_part == 1:
-            return {int(seed) for seed in re.findall(r"\d+", seed_str)}
-        else:
-            return sorted(
-                [
-                    Range(int(start), int(start) + int(range_len))
-                    for start, range_len in re.findall(r"(\d+) (\d+)", seed_str)
-                ],
-                key=lambda r: r.start,
-            )
-
-    def process_maps(self, maps_str):
-        if self.puzzle_part == 2:
-            maps = re.findall(r"map:\n([\d \n]+)\n\n", maps_str)
+            # return {int(seed) for seed in re.findall(r"\d+", seed_str)}
             return [
-                sorted(
-                    [Transform(*row) for row in transformation],
-                    key=lambda t: t.source_start,
-                )
-                for transformation in [
-                    [
-                        [int(x) for x in row.split()]
-                        for row in mapping.splitlines()
-                    ] for mapping in maps
-                ]
+                Range(int(start), int(start) + 1)
+                for start in re.findall(r"\d+", seed_str)
+            ]
+        else:
+            return [
+                Range(int(start), int(start) + int(range_len))
+                for start, range_len in re.findall(r"(\d+) (\d+)", seed_str)
             ]
 
-        def create_transform(mapping):
-            def transform(from_num):
-                for dest_range_start, source_range_start, range_len in (
-                        (int(num) for num in line.split())
-                        for line in mapping.split("\n")
-                ):
-                    if source_range_start <= from_num < source_range_start + range_len:
-                        return from_num - source_range_start + dest_range_start
-                return from_num
-
-            return transform
-
+    @staticmethod
+    def process_maps(maps_str):
         return [
-            create_transform(mapping)
-            for mapping in re.findall(r"map:\n([\d \n]+)\n\n", maps_str)
+            sorted(
+                [Transform(*row) for row in transformation],
+                key=lambda t: t.source_start,
+            )
+            for transformation in [
+                [
+                    [int(x) for x in row.split()]
+                    for row in mapping.splitlines()
+                ] for mapping in re.findall(r"map:\n([\d \n]+)\n\n", maps_str)
+            ]
         ]
 
     def process_input(self, content: str):
@@ -124,8 +81,9 @@ class Puzzle5Solver(Solver):
         maps = self.process_maps(maps_str)
         return seeds, maps
 
-    def update(self, seeds: list[Range], transforms: list[Transform]):
-        # seeds and transforms are both sorted
+    @staticmethod
+    def update(seeds: list[Range], transforms: list[Transform]):
+        # transforms are sorted
         new_seeds = []
         while seeds:
             seed = seeds.pop(0)
@@ -147,18 +105,11 @@ class Puzzle5Solver(Solver):
                 new_seeds.append(seed)
         return new_seeds
 
-
     def solve(self):
-        if self.puzzle_part == 1:
-            return min(
-                reduce(lambda acc, transform: transform(acc), self.maps, seed)
-                for seed in self.seeds
+        return min(
+            loc_range.start for loc_range in reduce(
+                self.update,
+                self.maps,
+                self.seeds,
             )
-        else:
-            return min(
-                loc_range.start for loc_range in reduce(
-                    self.update,
-                    self.maps,
-                    self.seeds
-                )
-            )
+        )
